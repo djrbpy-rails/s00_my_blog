e9c9c746dccbfea936856af133cae0e586929e2b
Moosavi.MirHassan <17009501+mir-moo@users.noreply.github.com>
2023-07-01 08:34:23 +0330

    test seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+10.times do |x|
+  Post.create(title: "Title #{x}", body: "this is post ##{x}'s contents...'")
+end
